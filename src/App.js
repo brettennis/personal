ce9b45055ed714b9366c2ab2bcbb37e5f3b8a068
Brett Ennis <brettennis4@gmail.com>
2023-09-26 22:25:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import icon_email from './icon-email.svg';
+import icon_github from './icon-github.svg';
+import icon_linkedin from './icon-linkedin.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+    	<div className='App'>
+
+			<ul className='logos'>
+				<li><a 
+					target="_blank" rel="noopener noreferrer"
+					href='mailto:brettennis4@gmail.com'>
+					<img className='logo' src={icon_email} alt='Email'></img>
+				</a></li>
+				<li><a 
+					target="_blank" rel="noopener noreferrer"
+					href='https://github.com/brettennis'>
+					<img className='logo' src={icon_github} alt='GitHub'></img>
+				</a></li>
+				<li><a 
+					target="_blank" rel="noopener noreferrer"
+					href='https://linkedin.com/in/brettennis'>
+					<img className='logo' src={icon_linkedin} alt='LinkedIn'></img>
+				</a></li>
+			</ul>
+
+			<div className='container'>
+				<main>
+
+				<div className='name'>
+					<h1>Brett Ennis</h1>
+				</div>
+
+				<div className='link-container'>
+					<ul className='labels'>
+						<li>Email</li>
+						<li>GitHub</li>
+						<li>LinkedIn</li>
+					</ul>
+					<ul className='links'>
+						<li><a 
+							target="_blank" rel="noopener noreferrer"
+							href='mailto:brettennis4@gmail.com'>
+							brettennis4@gmail.com
+						</a></li>
+						<li><a 
+							target="_blank" rel="noopener noreferrer"
+							href='https://github.com/brettennis'>
+							brettennis
+						</a></li>
+						<li><a 
+							target="_blank" rel="noopener noreferrer"
+							href='https://linkedin.com/in/brettennis'>
+							in/brettennis
+						</a></li>
+					</ul>
+				</div>
+
+				<div className='projects'>
+					<h2>Personal Projects</h2>
+				</div>
+
+				</main>
+			</div>
+    	</div>
+  	);
 }
 
 export default App;
